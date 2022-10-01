0e9328e2846dc0e9a02e5835f6feb91d14fc9595
brentaro <99369506+brentaro@users.noreply.github.com>
2022-09-30 20:01:26 -0700

    Add files via upload

---
+++ sample/config.h
@@ -0,0 +1,12 @@
+// 1000hz Polling Rate
+#define USB_POLLING_INTERVAL_MS 1
+#define QMK_KEYS_PER_SCAN 12
+
+// Joystick Button Count
+#define JOYSTICK_BUTTON_COUNT 32
+
+// Joystick Axes Count
+#define JOYSTICK_AXES_COUNT 6
+
+// Joystick Axes Resolution
+#define JOYSTICK_AXES_RESOLUTION 8
